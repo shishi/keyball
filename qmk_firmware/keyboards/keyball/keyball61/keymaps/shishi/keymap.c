a6d26d71fe4eb4e6e27123b2153e9f4d13725b37
Shigenobu Nishikawa <shishi@srevo.net>
2024-04-19 21:26:50 +0900

    mouse

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/shishi/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/shishi/keymap.c
@@ -15,7 +15,7 @@ void oledkit_render_info_user(void) {
 
 void pointing_device_init_user(void) {
     // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
-    // set_auto_mouse_layer(1);
+    set_auto_mouse_layer(1);
 
     // always required before the auto mouse feature will work
     set_auto_mouse_enable(true);
