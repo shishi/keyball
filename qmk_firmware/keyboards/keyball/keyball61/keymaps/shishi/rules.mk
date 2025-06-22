78f3dc34f710b07875bb0b41f8d06c30a7f4661d
Shigenobu Nishikawa <shishi@srevo.net>
2025-06-22 14:53:20 +0900

    fix

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/shishi/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/shishi/rules.mk
@@ -1,7 +1,7 @@
 CONSOLE_ENABLE = yes
 OLED_ENABLE = yes
 RGBLIGHT_ENABLE = no
-TAP_DANCE_ENABLE = yes
+TAP_DANCE_ENABLE = no
 VIA_ENABLE = no
 # sm_td https://github.com/stasmarkin/sm_td/blob/main/docs/010_installation_guide.md
 DEFERRED_EXEC_ENABLE = yes
