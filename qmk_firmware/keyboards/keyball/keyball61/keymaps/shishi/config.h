07680e44f8dd7484aa7fa80121984326c66f2246
Shigenobu Nishikawa <shishi@srevo.net>
2025-06-23 11:59:51 +0900

    disable permissive hold

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/shishi/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/shishi/config.h
@@ -8,8 +8,8 @@
 // enable after 0.24
 // #define AUTO_MOUSE_THRESHOLD 10
 
-#define TAPPING_TERM 145
-#define PERMISSIVE_HOLD
+#define TAPPING_TERM 140
+// #define PERMISSIVE_HOLD
 // #define HOLD_ON_OTHER_KEY_PRESS
 
 // // sm_td https://github.com/stasmarkin/sm_td/blob/main/docs/010_installation_guide.md
