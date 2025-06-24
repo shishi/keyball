967094615555905860bdff32089dbd020a227793
Shigenobu Nishikawa <shishi@srevo.net>
2025-06-24 22:22:42 +0900

    prepare to delete num line

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/shishi/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/shishi/keymap.c
@@ -1546,18 +1546,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX  , TO(3)        , TO(2)        , TO(1)        , LT(3,KC_ESC) , LT(2,KC_SPC) , LT(1,KC_TAB) ,          LT(1,KC_ENTER) , LT(2,KC_BSPC) , XXXXXXX      , XXXXXXX      , XXXXXXX        , XXXXXXX         , XXXXXXX
   ),
   [1] = LAYOUT_universal(
-    XXXXXXX , XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                                            XXXXXXX , XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
-    XXXXXXX , KC_PEQL  , KC_P7   , KC_P8   , KC_P9   , KC_PMNS ,                                            XXXXXXX , KC_HOME  , KC_END  , KC_PGUP , KC_GRV  , XXXXXXX ,
-    XXXXXXX , KC_PSLS  , KC_P4   , KC_P5   , KC_P6   , KC_PPLS ,                                            KC_LEFT , KC_DOWN  , KC_UP   , KC_RGHT , KC_QUOT , XXXXXXX ,
-    XXXXXXX , KC_PAST  , KC_P1   , KC_P2   , KC_P3   , KC_PENT , XXXXXXX ,              XXXXXXX , XXXXXXX , KC_MINUS , KC_EQL  , KC_PGDN , KC_BSLS , XXXXXXX ,
-    XXXXXXX , XXXXXXX  , KC_NUM  , KC_PDOT , KC_PCMM , KC_0    , TO(0)   ,              TO(0)   , KC_DEL  , XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
+    XXXXXXX , XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                                 XXXXXXX , XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , KC_PEQL  , KC_P7   , KC_P8   , KC_P9   , KC_PMNS ,                                 KC_APP  , KC_1     , KC_2    , KC_3    , KC_4    , XXXXXXX ,
+    XXXXXXX , KC_PSLS  , KC_P4   , KC_P5   , KC_P6   , KC_PPLS ,                                 KC_PSCR , KC_5     , KC_6    , KC_7    , KC_8    , XXXXXXX ,
+    XXXXXXX , KC_PAST  , KC_P1   , KC_P2   , KC_P3   , KC_PENT , XXXXXXX ,             XXXXXXX , XXXXXXX , KC_9     , KC_0    , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , XXXXXXX  , KC_NUM  , KC_PDOT , KC_PCMM , KC_0    , TO(0)   ,             TO(0)   , XXXXXXX , XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
   ),
   [2] = LAYOUT_universal(
-    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX  ,                                 XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX ,
-    XXXXXXX , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_INS   ,                                 KC_APP   , KC_1     , KC_2     , KC_3    , KC_4    , XXXXXXX ,
-    XXXXXXX , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_SCRL  ,                                 KC_PSCR  , KC_5     , KC_6     , KC_7    , KC_8    , XXXXXXX ,
-    XXXXXXX , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_PAUSE , XXXXXXX  ,            XXXXXXX , XXXXXXX  , KC_9     , KC_0     , XXXXXXX , XXXXXXX , XXXXXXX ,
-    XXXXXXX , KC_F13  , KC_F14  , KC_F15  , XXXXXXX , XXXXXXX  , TO(0)    ,            TO(0)   , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX  ,                                 XXXXXXX , XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_INS   ,                                 XXXXXXX , KC_HOME  , KC_END  , KC_PGUP , KC_GRV  , XXXXXXX ,
+    XXXXXXX , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_SCRL  ,                                 KC_LEFT , KC_DOWN  , KC_UP   , KC_RGHT , KC_QUOT , XXXXXXX ,
+    XXXXXXX , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_PAUSE , XXXXXXX  ,            XXXXXXX , XXXXXXX , KC_MINUS , KC_EQL  , KC_PGDN , KC_BSLS , XXXXXXX ,
+    XXXXXXX , KC_F13  , KC_F14  , KC_F15  , XXXXXXX , XXXXXXX  , TO(0)    ,            TO(0)   , KC_DEL  , XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
   ),
   [3] = LAYOUT_universal(
     XXXXXXX , XXXXXXX  , XXXXXXX , XXXXXXX  , XXXXXXX  , XXXXXXX ,                                XXXXXXX              , XXXXXXX    , XXXXXXX     , XXXXXXX , XXXXXXX , XXXXXXX  ,
