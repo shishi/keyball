9f66c5d9fa84088895d9f59aab6fef56c8801a2a
Shigenobu Nishikawa <shishi@srevo.net>
2025-10-15 01:01:50 +0900

    typo

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/shishi/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/shishi/keymap.c
@@ -1620,28 +1620,28 @@ combo_t key_combos[] = {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (shishi)
-  [0] = LAYOUT_right_ball(
+  [0] = LAYOUT_universal(
     KC_Q         , KC_W         , TD(TD_E)     , TD(TD_R)     , TD(TD_T)     ,                                       TD(TD_Y)      , TD(TD_U)     , TD(TD_I)     , TD(TD_O)       , TD(TD_P)        ,
     LGUI_T(KC_A) , LALT_T(KC_S) , LSFT_T(KC_D) , LCTL_T(KC_F) , TD(TD_G)     ,                                       TD(TD_H)      , RCTL_T(KC_J) , RSFT_T(KC_K) , RALT_T(KC_L)   , RGUI_T(KC_SCLN) ,
     TD(TD_Z)     , TD(TD_X)     , TD(TD_C)     , TD(TD_V)     , TD(TD_B)     ,                                       TD(TD_N)      , TD(TD_M)     , TD(TD_COMMA) , KC_DOT         , TD(TD_SLASH)    ,
     XXXXXXX      , KC_LNG1      , KC_LNG2      , LT(3,KC_ESC) , LT(2,KC_SPC) , LT(1,KC_TAB) ,       LT(1,KC_ENTER) , LT(2,KC_BSPC) , XXXXXXX      , XXXXXXX      , XXXXXXX        , XXXXXXX         ,
   ),
 
-  [1] = LAYOUT_right_ball(
+  [1] = LAYOUT_universal(
     KC_PEQL  , KC_P7   , KC_P8   , KC_P9   , KC_PMNS ,                         KC_APP  , KC_1     , KC_2    , KC_3     , KC_4    ,
     KC_PSLS  , KC_P4   , KC_P5   , KC_P6   , KC_PPLS ,                         KC_PSCR , KC_5     , KC_6    , KC_7     , KC_8    ,
     KC_PAST  , KC_P1   , KC_P2   , KC_P3   , KC_PENT ,                         XXXXXXX , KC_9     , KC_0    , KC_MINUS , KC_EQL  ,
     KC_NUM   , KC_PDOT , KC_PCMM , KC_0    , KC_0    , TO(0) ,       TO(0)   , XXXXXXX , XXXXXXX  , XXXXXXX , XXXXXXX  , XXXXXXX ,
   ),
 
-  [2] = LAYOUT_right_ball(
+  [2] = LAYOUT_universal(
     KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_INS   ,                         KC_PGUP , KC_HOME  , KC_END  , KC_PGUP , KC_GRV  ,
     KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_SCRL  ,                         KC_LEFT , KC_DOWN  , KC_UP   , KC_RGHT , KC_QUOT ,
     KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_PAUSE ,                         KC_PGDN , KC_MINUS , KC_EQL  , KC_PGDN , KC_BSLS ,
     KC_F13  , KC_F14  , KC_F15  , XXXXXXX , XXXXXXX  , TO(0) ,       TO(0)   , KC_DEL  , XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX ,
   ),
 
-  [3] = LAYOUT_right_ball(
+  [3] = LAYOUT_universal(
     KBC_RST  , XXXXXXX , SSNP_HOR , SSNP_VRT , AML_TO  ,                          A(KC_F4)             , A(KC_LEFT) , A(KC_RIGHT) , XXXXXXX , KC_GRV  ,
     KBC_SAVE , XXXXXXX , CPI_D100 , CPI_I100 , QK_BOOT ,                          C(KC_W)              , KC_BTN1    , KC_BTN3     , KC_BTN2 , KC_QUOT ,
     XXXXXXX  , XXXXXXX , SCRL_DVD , SCRL_DVI , EE_CLR  ,                          C(S(KC_T))           , KC_PGUP    , KC_PGDN     , XXXXXXX , KC_BSLS ,
