2e4368e507e3219788fcbb8e37e94ad25d71e127
Shigenobu Nishikawa <shishi@srevo.net>
2026-03-20 02:18:13 +0900

    more tapping term for a and ;

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/shishi/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/shishi/keymap.c
@@ -1726,6 +1726,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
+// 特定のキーだけ TAPPING_TERM を個別に設定する
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LGUI_T(KC_A):
+        case RGUI_T(KC_SCLN):
+            return 300;
+        default:
+            return TAPPING_TERM;
+    }
+}
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 2);
