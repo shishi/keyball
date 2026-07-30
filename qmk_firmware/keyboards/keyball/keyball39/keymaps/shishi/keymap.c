3b79adc872f158339a3bd2447cf41c7a91f97906
Shigenobu Nishikawa <shishi@srevo.net>
2026-07-30 18:22:51 +0900

    number layer

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/shishi/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/shishi/keymap.c
@@ -1706,7 +1706,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_universal(
     KC_PEQL  , KC_P7   , KC_P8   , KC_P9   , KC_PMNS ,                         KC_1    , KC_2     , KC_3    , KC_4     , KC_5    ,
     KC_PSLS  , KC_P4   , KC_P5   , KC_P6   , KC_PPLS ,                         KC_6    , KC_7     , KC_8    , KC_9     , KC_0    ,
-    KC_PAST  , KC_P1   , KC_P2   , KC_P3   , KC_PENT ,                         KC_APP  , XXXXXXX  , XXXXXXX , XXXXXXX  , XXXXXXX ,
+    KC_PAST  , KC_P1   , KC_P2   , KC_P3   , KC_PENT ,                         KC_APP  , KC_MINUS , KC_EQL  , KC_COMMA , KC_DOT  ,
     KC_NUM   , KC_PDOT , KC_PCMM , KC_0    , KC_0    , TO(0) ,       TO(0)   , KC_PSCR , XXXXXXX  , XXXXXXX , XXXXXXX  , XXXXXXX
   ),
 
